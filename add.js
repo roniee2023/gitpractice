aa60ac1cd40ae2c81061cafd94fc3cb76537d482
binoy <binoykdasgupta@gmail.com>
2023-05-07 19:43:51 +0100

    functions added

--- add.js
+++ add.js
@@ -1,13 +1,19 @@
-let number=100;
-function add( x, y) {
-    return x +y+number;
+let number = 100;
+function add(x, y) {
+    return x + y + number;
 }
-console.log(add(10,25));
-function sub(x ,y) {
-    return x-y +number;
+console.log(add(10, 25));
+function sub(x, y) {
+    return x - y + number;
 }
-console.log(sub(45,55));
-function mul(x,y) {
-    return x*y+number;
-    }
-console.log(mul(10,20));    
+console.log(sub(45, 55));
+function mul(x, y) {
+    return x * y + number;
+}
+console.log(mul(10, 20));
+
+const adivision = function division(a, b) {
+    return (a / b) + number;
+}
+
+console.log(adivision(10, 5));
