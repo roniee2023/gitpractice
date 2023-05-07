26758b0a41267d278e2510481c1332e2e2b4cdc1
roniee2023 <me.roniee@gmail.com>
2023-05-07 19:06:32 +0100

    add mul function

--- add.js
+++ add.js
@@ -1,11 +1,13 @@
-
 let number=100;
 function add( x, y) {
     return x +y+number;
 }
-//console.log(add(10,25));
-
+console.log(add(10,25));
 function sub(x ,y) {
     return x-y +number;
 }
 console.log(sub(45,55));
+function mul(x,y) {
+    return x*y+number;
+    }
+console.log(mul(10,20));    
